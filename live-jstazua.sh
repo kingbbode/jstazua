8aa09e27cf48ebdaf3ab6771ccc844cdfaf036e8
kingbbode <kingbbode@gmail.com>
2018-03-19 00:24:40 +0900

    wirte live-jstazua init ver

---
+++ live-jstazua.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+which jps &> /dev/null
+if [ "$?" != "0" ]; then
+    echo "jps is not installed..."
+    exit 1
+fi
+
+which jstack &> /dev/null
+if [ "$?" != "0" ]; then
+    echo "jstack is not installed..."
+    exit 1
+fi
+
+if [ -z $1  ] || [ -z $2 ]; then
+  echo "usage> live-jstazua [application_name] [per_time] [folder_nmae_alias:optional]"
+  echo "ex) list-jstazua jstack-service 1m --> per 1 minute"
+  exit 1
+fi
+
+application=$1
+per=$2
+alias=$3
+pid=$(jps | grep '$application' | cut -f1 -d' ')
+
+if [[ -z $pid ]]; then
+  echo "could't find jps : $application"
+  exit 1
+fi
+
+dir=$(date +%m%d%H%M%S)_$alias
+
+if [[ -f dir ]]; then
+  echo "already exist folder($dir). please try again"
+  exit 1
+else
+  echo "create directory $dir"
+  mkdir $dir
+fi
+
+echo "****************************************************"
+echo "****************** JSTACK GAZUA ********************"
+echo "***************** RECODING  MODE *******************"
+echo "****************************************************"
+
+init=0
+
+while : ; do
+  init=$((init+1))
+  jstack $pid > $dir/$init.log
+  echo "create complated  jstack $init th."
+  ./jstazua.sh $dir/$init.log
+  echo "wait $per........"
+  sleep $per
+done
